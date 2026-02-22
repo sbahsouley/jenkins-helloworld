f29482818e5fb3a95a6b90135e78803829bb2b7c
sbahsouley <souley062002@gmail.com>
2026-02-22 15:09:01 +0100

    Clean up Main.java by removing shell commands
    Removed shell commands for creating and compiling Java file.

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
-mkdir -p /tmp/sbah
-rm -rf /tmp/sbah
-mkdir -p /tmp/sbah
-
-cat << 'EOF' > /tmp/sbah/Main.java
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
-EOF
 
-javac /tmp/sbah/Main.java
