31a663a442211e6e8ecaf25bacd49e0898657ce4
sbahsouley <souley062002@gmail.com>
2026-02-23 09:10:07 +0100

    Fix greeting message format in Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! c'est la part de Mr sbah");
+        System.out.println("Hello, World c'est la part de Mr sbah!");
     }
 }
 
